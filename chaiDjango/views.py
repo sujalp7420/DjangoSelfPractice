5cc39e187995c031f5391917fc2b23d6450b5e8d
sujalp7420 <sujalpatel2726@gmail.com>
2026-02-07 19:00:42 +0530

    version 2 with templates

--- chaiDjango/views.py
+++ chaiDjango/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello home")
+    # return HttpResponse("Hello home")
+    return render(request,'index.html')
+
 def about(request):
     return HttpResponse("Hello about")
+
 def contact(request):
     return HttpResponse("Hello contact")
